4383ea2640e2314c87e69a84e23b39e23e7bf720
Mani Movassagh <mani.mgh@gmail.com>
2024-12-13 00:03:43 +0100

    docker

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { getDBConnection } from './db/db';
 
 
 const app = express();
@@ -7,15 +6,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-getDBConnection().then(() => {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT} and connected to DB`);
-  });
-}).catch(err => {
-  console.error("Failed to connect to the database:", err);
-  process.exit(1);
-});
 
 
 app.get('/', (_req, res) => {
